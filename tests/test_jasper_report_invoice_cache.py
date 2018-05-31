64ccbe26879e1bbcbbcea96b8ad9f0e0f5061732
Albert Cervera i Areny <albert@nan-tic.com>
2018-05-31 16:13:16 +0200

    Use the right module name in tests.

--- tests/test_jasper_report_invoice_cache.py
+++ tests/test_jasper_report_invoice_cache.py
@@ -3,20 +3,14 @@
 import unittest
 # import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
-# TODO: Remove if no sceneario needed.
-# from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import test_depends
 
 
 class TestCase(unittest.TestCase):
     'Test module'
 
     def setUp(self):
-        trytond.tests.test_tryton.install_module('jasper_report_invoice_cache')
-
-    def test0005views(self):
-        'Test views'
-        test_view('jasper_report_invoice_cache')
+        trytond.tests.test_tryton.install_module('account_invoice_jreport_cache')
 
     def test0006depends(self):
         'Test depends'
@@ -26,8 +20,4 @@ class TestCase(unittest.TestCase):
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestCase))
-    # TODO: remove if no scenario needed.
-    #suite.addTests(doctest.DocFileSuite('scenario_jasper_report_invoice_cache.rst',
-    #        setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
-    #        optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
